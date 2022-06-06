fa4c3c168b635658dfb4fecafc78e859484a3fb4
Jae Hun Choi <chlwogns316@gmail.com>
2022-06-06 19:24:22 +0900

    ~22.06.06

---
+++ src/Algorithm_BOJ_Java/Step_1/BOJ_10718.java
@@ -0,0 +1,9 @@
+package Algorithm_BOJ_Java.Step_1;
+
+public class BOJ_10718 {
+    public static void main(String[] args) {
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
