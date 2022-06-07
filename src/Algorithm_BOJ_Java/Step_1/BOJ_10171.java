21e79949ccf3c29c4899c8dbeb5ec1d50fcbd707
Jae Hun Choi <chlwogns316@gmail.com>
2022-06-07 19:13:46 +0900

    ~22.06.07

---
+++ src/Algorithm_BOJ_Java/Step_1/BOJ_10171.java
@@ -0,0 +1,14 @@
+package Algorithm_BOJ_Java.Step_1;
+
+public class BOJ_10171 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+
+}
---
+++ src/Algorithm_BOJ_Java/Step_1/BOJ_10172.java
@@ -0,0 +1,14 @@
+package Algorithm_BOJ_Java.Step_1;
+
+public class BOJ_10172 {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
