984751c74bd78d047a69be0ac3c78be47159b816
Jae Hun Choi <chlwogns316@gmail.com>
2022-06-08 14:25:18 +0900

    ~22.06.08

---
+++ src/Algorithm_BOJ_Java/Step_1/BOJ_1000.java
@@ -0,0 +1,19 @@
+package Algorithm_BOJ_Java.Step_1;
+import java.util.Scanner;
+
+public class BOJ_1000 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        if (0 < A && B <10) {
+            System.out.println(A+B);
+        }
+        else {
+            System.out.println("범위 밖입니다.");
+        }
+    }
+}
