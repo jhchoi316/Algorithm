dd03fa3f22662ef47c5cc20efc1a1fdbd771fad0
Jae Hun Choi <chlwogns316@gmail.com>
2022-06-13 12:31:53 +0900

    ~22.06.13

---
+++ src/Algorithm_BOJ_Java/Step_2/BOJ_2753.java
@@ -0,0 +1,24 @@
+package Algorithm_BOJ_Java.Step_2;
+import java.util.Scanner;
+
+public class BOJ_2753 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if(year>4000)
+        {
+            System.out.println("범위 밖입니다.");
+        }
+        else if((year%4==0 && year%100!=0) || (year%4==0 && year%400==0))
+        {
+            System.out.println("1");
+        }
+        else
+        {
+            System.out.println("0");
+        }
+    }
+}
---
+++ src/Algorithm_BOJ_Java/Step_2/BOJ_9498.java
@@ -0,0 +1,32 @@
+package Algorithm_BOJ_Java.Step_2;
+import java.util.Scanner;
+
+public class BOJ_9498 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int grade = scanner.nextInt();
+
+        if (grade>=90 && grade<=100)
+        {
+            System.out.println("A");
+        }
+        else if (grade>=80 && grade<90)
+        {
+            System.out.println("B");
+        }
+        else if (grade>=70 && grade<80)
+        {
+            System.out.println("C");
+        }
+        else if (grade>=60 && grade<70)
+        {
+            System.out.println("D");
+        }
+        else
+        {
+            System.out.println("F");
+        }
+    }
+}
