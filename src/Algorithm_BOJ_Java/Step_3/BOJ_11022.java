a2c2ff1a79580d169a10c95cd3fb99d6432048e2
Jae Hun Choi <chlwogns316@gmail.com>
2022-06-22 23:28:58 +0900

    ~22.06.22

---
+++ src/Algorithm_BOJ_Java/Step_3/BOJ_11022.java
@@ -0,0 +1,31 @@
+package Algorithm_BOJ_Java.Step_3;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_11022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        int sum = 0;
+
+        for(int i=1;i<=T;i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + A + " + " + B + " = " + (A + B) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
